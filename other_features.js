91e8ae89e7a9e9c582b8027591a9efe6fcb8c4a4
Ishan3131 <chetwanikrish@gmail.com>
2025-04-10 23:56:27 +0530

    Add files via upload

---
+++ other_features.js
@@ -0,0 +1,24 @@
+//---------------------------------------------Cursor co-ordinates------------------------------------------------------------//
+let X, Y;
+document.addEventListener('mousemove', (e) => {
+  if (e.clientX < graphPage.width) {
+    X = (e.clientX - graphPage.width / 2) / 10;
+    Y = (graphPage.height / 2 - e.clientY) / 10;
+    document.querySelector('#cursor_x_cordinate').value = X;
+    document.querySelector('#cursor_y_cordinate').value = Y;
+  }
+})
+//-------------------------------------defaut input function---------------------------------------------------------------//
+function default_input() {
+    document.querySelector('#domain_start').value = -1 * Math.ceil(scale * graphPage.width / 20);
+    document.querySelector('#domain_end').value = Math.ceil(scale * graphPage.width / 20);
+    document.querySelector('#rate').value = scale / 10;
+}
+//----------------------------------------Clear log function-------------------------------------------------------------------------//
+function clearLog() {
+  graph_log_appear();
+  keyss = Object.keys(graphs);
+  keyss.forEach((key) => {
+      graphs[key].removee()
+  })
+}
